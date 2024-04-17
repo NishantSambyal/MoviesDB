558208e0f845fa5bd60a579934c9d5430ef31213
NishantSambyal <nishantsambyal123@gmail.com>
2024-04-17 13:25:22 +0530

    mock created

--- src/screens/authenticated/Dashboard/__tests__/Dashboard.test.tsx
+++ src/screens/authenticated/Dashboard/__tests__/Dashboard.test.tsx
@@ -1,61 +1,53 @@
 import '@testing-library/jest-native/extend-expect';
 import { render, waitFor } from '@testing-library/react-native';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import * as reactRedux from 'react-redux';
 import Dashboard from '..';
 import { getMovies } from '../dataController';
 
-// Mocking dataController
 jest.mock('../dataController');
 
-// Mocking useSelector and useDispatch
 jest.mock('react-redux');
 
 describe('Dashboard Component', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-
+  const mockedMoviesContext = getMovies as jest.Mock<any>;
+  const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
   test('renders DashboardScreen with initial movie list', async () => {
-    // Mock useSelector to return a language
-    useSelector.mockReturnValue('en');
+    useSelectorMock.mockReturnValue('en');
 
     // Mock initial movies
     const initialMovies = [
-      { id: 1, title: 'Movie 1' },
-      { id: 2, title: 'Movie 2' },
+      { id: 1, title: 'Movie 1', poster_path: 'path/to/movie1.jpg' },
+      { id: 2, title: 'Movie 2', poster_path: 'path/to/movie2.jpg' },
     ];
-    getMovies.mockResolvedValueOnce({ results: initialMovies });
+    mockedMoviesContext.mockResolvedValueOnce({ results: initialMovies });
 
     const { getByTestId } = render(<Dashboard />);
 
-    // Wait for DashboardScreen to render
     await waitFor(() =>
       expect(getByTestId('dashboard-component')).toBeTruthy(),
     );
 
-    // Check if initial movies are rendered
     initialMovies.forEach(movie => {
       expect(getByTestId(`movie-item-${movie.id}`)).toBeTruthy();
     });
   });
 
   test('calls console.log when movies fetch fails', async () => {
-    // Mock useSelector to return a language
-    useSelector.mockReturnValue('en');
+    useSelectorMock.mockReturnValue('en');
 
-    // Mock rejected promise for getMovies
     const errorMessage = 'Failed to fetch movies';
-    getMovies.mockRejectedValueOnce(errorMessage);
+    mockedMoviesContext.mockRejectedValueOnce(errorMessage);
 
     const consoleSpy = jest.spyOn(console, 'log');
 
     render(<Dashboard />);
 
-    // Wait for a short time to allow for potential console log
     await waitFor(() => {}, { timeout: 500 });
 
-    // Check if console log is called with the error message
     expect(consoleSpy).toHaveBeenCalledWith('error', errorMessage);
   });
 });
